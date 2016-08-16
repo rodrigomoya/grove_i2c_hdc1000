4470abb192f85e9236585297c1c465049900d355
rodrigomoya <rodrigo@rizoma.io>
2016-08-16 14:27:44 -0300

    fix

--- grove_i2c_temp_humi_hdc1000.cpp
+++ grove_i2c_temp_humi_hdc1000.cpp
@@ -101,26 +101,29 @@ bool GroveI2CTempHumiHdc1000::read_humidity(float *humidity)
     double humi = dest;
     humi_t = ((humi/65536.0)*100.0);
 
-    if ((humi_t >= 20.0) && (humi_t <= 30.0)) {      
+    if ((humi_t >= 20.00) && (humi_t <= 30.00)) {      
       // - 1
-      offset = 1.0;
-    } else if ((humi_t >= 31.0) && (humi_t <= 40.0)) {      
+      offset = 1.00;
+    } else if ((humi_t >= 31.00) && (humi_t <= 40.00)) {      
       // - 5
-      offset = 5.0;
-    } else if ((humi_t >= 41.0) && (humi_t <= 50.0)) {      
+      offset = 5.00;
+    } else if ((humi_t >= 41.00) && (humi_t <= 50.00)) {      
       // - 10    
-      offset = 10.0;
-    } else if ((humi_t >= 51.0) && (humi_t <= 60.0)) {      
+      offset = 10.00;
+    } else if ((humi_t >= 51.00) && (humi_t <= 60.00)) {      
       // - 15 
-      offset = 15.0;
-    } else if ((humi_t >= 71.0) && (humi_t <= 80.0)){      
+      offset = 12.50;
+    } else if ((humi_t >= 61.00) && (humi_t <= 70.00)) {      
+      // - 15       
+      offset = 15.00;
+    } else if ((humi_t >= 71.00) && (humi_t <= 80.00)){      
       // - 10
-      offset = 10.0;
-    } else if ((humi_t >= 81.0) && (humi_t <= 90.0)){                
+      offset = 10.00;
+    } else if ((humi_t >= 81.00) && (humi_t <= 90.00)){                
       // - 5      
-      offset = 5.0;
+      offset = 5.00;
     } else {
-      offset = 0.0;
+      offset = 0.00;
     }
 
     *humidity = ((humi/65536.0)*100.0)-offset;
